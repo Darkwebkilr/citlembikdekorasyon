e137cae02270174c77cf6f3773d5757b950407e5
Darkwebkilr <ismethaktan39@gmail.com>
2025-08-31 22:17:47 +0000

    adas

---
+++ src/types.ts
@@ -0,0 +1,40 @@
+export interface Features {
+  link?: string;
+  title: string;
+  description: string;
+}
+
+export const feature: Features[] = [
+  {
+    link: "/hizmetler/adan-zye-dekorasyon",
+    title: "A' dan Z'ye Ev dekorasyon",
+    description: "Fikirlerinizi dinliyor, ihtiyaçlarınıza özel, estetik ve fonksiyonel tasarımlar hazırlıyoruz.",
+  },
+  {
+    title: "3 Boyutlu Tasarım",
+    description: "Projenizin gereksinimlerine en uygun, uzun ömürlü ve kaliteli malzemeleri özenle seçiyoruz.",
+  },
+  {
+    title: "Mobilya Üretim Ve Tasarım",
+    description: "Onaylanan tasarımları, en kaliteli malzemelerle hayata geçiriyoruz.",
+  },
+  {
+    title: "Özel Dolap Tasarım",
+    description: "Üretimin her aşamasında titiz bir kalite kontrol uygulayarak kusursuz ürünler ortaya çıkarıyoruz.",
+  },
+  {
+    title: "Yatak Tasarım Ve Üretim",
+    description: "Ürünlerinizi özenle paketliyor, adresinize teslim ediyor ve profesyonel montajını gerçekleştiriyoruz.",
+  },
+  {
+    title: "Ahşap İşleri",
+    description: "Teslimat sonrası her türlü sorunuz ve ihtiyacınız için yanınızda olmaya devam ediyoruz.",
+  },
+  {
+    title: "Özel Üretim Masa",
+    description: "Üretimin her aşamasında titiz bir kalite kontrol uygulayarak kusursuz ürünler ortaya çıkarıyoruz.",
+  },
+
+]
+
+
